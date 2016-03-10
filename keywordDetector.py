5adf5ddd20f3a1bc1fc29cfbddaf184f9bdec12e
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2016-03-10 17:31:17 -0500

    Fix indentation to tab.

--- keywordDetector.py
+++ keywordDetector.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
         sys.exit(0)
 
     CLIENT_KEY = sys.argv[1]
-	CLIENT_ID = sys.argv[2]
+    CLIENT_ID = sys.argv[2]
 
     kd = KeywordDetector(CLIENT_KEY, CLIENT_ID)
     kd.start()
